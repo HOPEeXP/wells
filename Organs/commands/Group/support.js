4c9ba6e666e7018329cf91a929216fa3bc129d8a
HOPE <101519089+HOPEeXP@users.noreply.github.com>
2023-02-09 14:26:28 +0530

    Update support.js

--- Organs/commands/Group/support.js
+++ Organs/commands/Group/support.js
@@ -3,13 +3,13 @@ module.exports = {
 	alias: ["sup"],
 	desc: "Send you official support group link.",
 	category: "Group",
-    react:"✅",
+    react:"📡",
     start:async(client,m,{command,prefix,pushName})=>{
-        let yup = ['https://media.tenor.com/XikoNQDfaqcAAAPo/bunny-nezuko-bunny-ears.mp4',
-                   'https://media.tenor.com/xBh07rz9GHYAAAPo/nezuko-kamado-nezuko.mp4',
-                   'https://media.tenor.com/CMHP0DIQY4UAAAPo/demon-slayer-kimetsu-no-yaiba.mp4',
-                   'https://media.tenor.com/68c6pxzq3RAAAAPo/nezuko-nezuko-kamado.mp4',
-                   'https://media.tenor.com/JQgNUfM_xcwAAAPo/nezuko-kamado-run.mp4'
+        let yup = ['https://media.tenor.com/znKENedN06oAAAPo/harry-wells-wink.mp4',
+                   'https://media.tenor.com/znKENedN06oAAAPo/harry-wells-wink.mp4',
+                   'https://media.tenor.com/znKENedN06oAAAPo/harry-wells-wink.mp4',
+                   'https://media.tenor.com/znKENedN06oAAAPo/harry-wells-wink.mp4',
+                   'https://media.tenor.com/znKENedN06oAAAPo/harry-wells-wink.mp4'
                   ]
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
@@ -20,14 +20,14 @@ const eternitylogogg = "https://i.ibb.co/t3Jd1wK/Eternity-6.png"
 const lemo = `
 *━『 Support Group Links 』━*
 
-*1) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ] :*
+*1) [ ▼△▼△ 𝚃𝙾𝙼𝙰𝙽 ▼△▼△ ] :*
+https://chat.whatsapp.com/Hxpr3MtH79CIpGF4vG7NZ7
+
+*2) [ ⭑⭑⭑★ 𝐁𝐎𝐍𝐓𝐄𝐍 ★⭑⭑⭑ ]  :*
+https://chat.whatsapp.com/CQKbWP7c3GNIk4SfE7Xej7
+
+*3) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ  ] :*
 https://chat.whatsapp.com/JCCZPbPUbM1536n62zSFZi
-
-*2) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ₵𝖆𝖘𝖎𝖓ø ] 🎰 :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
-
-*3) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ N̸s̸f̸w̸ ] :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
 `
   await client.sendMessage(m.from,{video:{url:rae}, gifPlayback:true, caption: `*${pushName}* have a look in your DM`},{quoted:m})
   await  client.sendMessage(m.sender,{image:{url:eternitylogogg},caption:lemo},{quoted:m})  
