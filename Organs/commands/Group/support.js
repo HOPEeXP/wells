fbd077e4f79720cae47be0164d7af6a9dd98c2fb
HOPE <101519089+HOPEeXP@users.noreply.github.com>
2023-02-09 14:38:34 +0530

    Update support.js

--- Organs/commands/Group/support.js
+++ Organs/commands/Group/support.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "support",
-	alias: ["sup"],
+	alias: ["wells"],
 	desc: "Send you official support group link.",
 	category: "Group",
     react:"📡",
@@ -14,7 +14,7 @@ module.exports = {
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
   
-const eternitylogogg = "https://i.ibb.co/t3Jd1wK/Eternity-6.png"
+const eternitylogogg = "https://media.tenor.com/8_KQ72LtiiIAAAPo/harry-wells-harrison-wells.mp4"
         
                          
 const lemo = `
@@ -26,8 +26,6 @@ https://chat.whatsapp.com/Hxpr3MtH79CIpGF4vG7NZ7
 *2) [ ⭑⭑⭑★ 𝐁𝐎𝐍𝐓𝐄𝐍 ★⭑⭑⭑ ]  :*
 https://chat.whatsapp.com/CQKbWP7c3GNIk4SfE7Xej7
 
-*3) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ  ] :*
-https://chat.whatsapp.com/JCCZPbPUbM1536n62zSFZi
 `
   await client.sendMessage(m.from,{video:{url:rae}, gifPlayback:true, caption: `*${pushName}* have a look in your DM`},{quoted:m})
   await  client.sendMessage(m.sender,{image:{url:eternitylogogg},caption:lemo},{quoted:m})  
